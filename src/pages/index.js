256999e399b786226303d360e6913d6e528dad7d
STAN LEE <>
2021-05-10 02:43:45 -0500

    add projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Services from '../components/Services';
 import Jobs from '../components/Jobs';
 import Projects from '../components/Projects';
 import Seo from '../components/Seo';
+
 const IndexPage = ({ data }) => {
   const {
     allStrapiProject: { nodes: projects },
@@ -46,4 +47,5 @@ export const query = graphql`
     }
   }
 `;
+
 export default IndexPage;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,13 +1,48 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Projects from "../components/Projects"
-import Seo from "../components/Seo"
-const ProjectsPage = () => {
+import React from 'react';
+import { graphql } from 'gatsby';
+import Projects from '../components/Projects';
+import Seo from '../components/Seo';
+
+const ProjectsPage = ({
+  data: {
+    allStrapiProject: { nodes: projects },
+  },
+}) => {
   return (
     <>
-      <h2>projects page</h2>
+      <main>
+        <section className="projects-page">
+          <Projects title="all projects" projects={projects} />
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default ProjectsPage
+export const query = graphql`
+  {
+    allStrapiProject {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`;
+
+export default ProjectsPage;
